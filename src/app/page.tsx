261f5e9d1aa3b67708e28ce248b764504a67e0e8
long <liw.polaris@gmail.com>
2025-11-04 14:48:42 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import CouponCard from "../components/CouponCard";
 import UnsubscribeForm from "../components/UnsubscribeForm";
 import InstallBanner from "../components/InstallBanner";
